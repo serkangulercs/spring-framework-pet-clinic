3bf84f3949341b7758d1065e7320f760e2a1de3b
serkan <serkanguler.cs@gmail.com>
2021-05-29 13:10:20 +0300

    Adding spring data jpa service for vets. Closes #41.

--- pet-clinic-data/src/main/java/com/springframework/petclinic/service/springdatajpa/OwnerJpaService.java
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/service/springdatajpa/OwnerJpaService.java
@@ -12,7 +12,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Service
-@Profile("springdatajpa")
+@Profile("spring-data-jpa")
 public class OwnerJpaService implements OwnerService {
 
     private final OwnerRepository ownerRepository;
---
+++ pet-clinic-data/src/main/java/com/springframework/petclinic/service/springdatajpa/VetJpaService.java
@@ -0,0 +1,48 @@
+package com.springframework.petclinic.service.springdatajpa;
+
+import com.springframework.petclinic.model.Vet;
+import com.springframework.petclinic.repository.VetRepository;
+import com.springframework.petclinic.service.VetService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("spring-data-jpa")
+public class VetJpaService implements VetService {
+
+    private final VetRepository vetRepository;
+
+    public VetJpaService(VetRepository vetRepository) {
+        this.vetRepository = vetRepository;
+    }
+
+    @Override
+    public Set<Vet> findAll() {
+        Set<Vet> vets = new HashSet<>();
+        vetRepository.findAll().forEach(vets::add);
+        return vets;
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return vetRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return vetRepository.save(object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        vetRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        vetRepository.deleteById(id);
+    }
+}
